9b3a5b654762822a0c98515d848e9e1c7da46c0e
Alexander Jahn <jahn.alexander@gmail.com>
2020-05-24 18:08:37 +0200

    initial commit

---
+++ simple_model.py
@@ -0,0 +1,33 @@
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Convolution2D, MaxPooling2D
+from keras.optimizers import Adam
+from keras.utils import np_utils
+from sklearn import metrics
+
+def create_model():
+    num_labels = 2
+    filter_size = 2
+
+    # Construct model
+    model = Sequential()
+
+    model.add(Dense(256, input_shape=(80,)))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+
+    model.add(Dense(256))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+
+    model.add(Dense(num_labels))
+    model.add(Activation('softmax'))
+
+    model.compile(loss='categorical_crossentropy', metrics=['accuracy'], optimizer='adam')
+    return model
+
+def load_trained_model(weights_path):
+    model = create_model()
+    model.load_weights(weights_path)
+    return model
---
+++ simple_model_feature.py
@@ -0,0 +1,15 @@
+import librosa
+import numpy as np
+import soundfile as sf
+
+def extract_features(audio, sample_rate, f=None):
+    if f:
+        audio, sample_rate = librosa.load(f,mono=False)
+
+    mfccs1 = librosa.feature.mfcc(y=np.asfortranarray(audio[0,:]), sr=sample_rate, n_mfcc=40)
+    mfccsscaled1 = np.mean(mfccs1.T,axis=0)
+
+    mfccs2 = librosa.feature.mfcc(y=np.asfortranarray(audio[1,:]), sr=sample_rate, n_mfcc=40)
+    mfccsscaled2 = np.mean(mfccs2.T,axis=0)
+
+    return np.hstack((mfccsscaled1, mfccsscaled2))
